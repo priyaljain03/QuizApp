02b2eed9ce1b1487b95ec6af4497008d22cec0cb
Priyal <jainpriyal03@gmail.com>
2021-11-12 01:24:48 +0530

    Added logic to add questions and answer on quiz page and viewing random number of questions

--- quizes/static/quizes/quiz.js
+++ quizes/static/quizes/quiz.js
@@ -2,12 +2,34 @@ console.log("Hello world quiz")
 const url = window.location.href
 console.log(url)
 
+const quizBox = document.getElementById('quiz-box')
+
+
 
 $.ajax({
     type: 'GET',
     url: `${url}data`,
     success: function(response){
         console.log(response)
+        const data = response.data
+        data.forEach(ele=>{
+            for(const [question,answers] of Object.entries(ele)){
+                quizBox.innerHTML += `
+                <hr>
+                <div class = "mb-2">
+                <b>${question}</b>
+               
+                </div>
+                `
+                answers.forEach(answer=>{
+                    quizBox.innerHTML += `
+                    <div>
+                        <input type="radio" class="ans" id="${question}-${answer}" name="${question}"value="${answer}" >
+                        <label for="${question}">${answer}</label>
+                    </div>`
+                })
+            }
+        })
     },
     error: function(response){
         console.log(error)
