25e60c943ac4165980332ef1e78a038b109f9147
Priyal <jainpriyal03@gmail.com>
2021-11-12 00:16:37 +0530

    created view for extracting questions and related answers

--- quizes/views.py
+++ quizes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Quiz
+from questions.models import Question
 from django.views.generic import ListView
+from django.http import JsonResponse
 # Create your views here.
 
 
@@ -8,7 +10,22 @@ class QuizListView (ListView):
     model = Quiz
     template_name = 'quizes/main.html'
 
+
+#Detail view
 def quiz_view(request,pk):
     quiz = Quiz.objects.get(pk=pk)
     return render(request,'quizes/quiz.html',{'obj':quiz})
+
+def quiz_data_view(request,pk):
+    quiz = Quiz.objects.get(pk=pk)
+    questions =[]
+    for q in quiz.get_questions():
+        answers = []
+        for a in q.get_answer():
+            answers.append(a.text)
+        questions.append({str(q):answers})
+    return JsonResponse({
+        'data': questions,
+        'time': quiz.time
+    })
 
